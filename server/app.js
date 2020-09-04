a0195a5d78094d4cd3b0acbf160c1771f1029403
dnuwa <daniel.nuwa@gmail.com>
2020-09-04 22:35:52 +0300

    - adds cors and the morgan dev dependecy

--- server/app.js
+++ server/app.js
@@ -2,10 +2,16 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 // console.log(typeof(graphqlHTTP))
 const mongoose = require("mongoose");
-// const {MongoClient} = require('mongodb');
+const cors = require('cors');
+const morgan = require('morgan');
+
 const schema = require("./schema/schema");
 
 const app = express();
+
+//allow cross-origin requests
+app.use(cors());
+app.use(morgan('combined'));
 
 const uri =
   "mongodb+srv://dnuwa:@Microsoft%231@cluster0.czpl5.mongodb.net/graphql-playlist?retryWrites=true&w=majority";
